e0790ad9e3e6c2e5f406ba9691cf499734220cee
Thomas Ames <tham7107@colorado.edu>
2023-07-08 12:23:41 -0600

    Initial commit

---
+++ finder-app/writer.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+######################################################################
+##
+## Thomas Ames
+## ECEA 5305, assignment #1, writer.sh
+## July 2023
+##
+
+# $# is the # of args not including the filename ($0)
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 writefile writestr"
+    echo "Create the file writefile containing text writestr."
+    echo "Overwrites writefile if already exists."
+    echo "Exit code 0 on success, 1 if file cannot be written or bad args"
+    exit 1
+fi
+
+# For clarity in script, use descriptive variable names
+writefile=$1
+writestr=$2
+
+echo $writestr > $writefile
+
+# Check result of echo
+if [ $? -ne 0 ]; then
+    echo "Error writing to $writefile"
+    exit 1
+fi
+
+exit 0
