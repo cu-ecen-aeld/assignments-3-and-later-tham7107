d4dc04791ec91c4f393cda5bd2fb571d19a196e0
Thomas Ames <tham7107@colorado.edu>
2023-07-10 09:27:52 -0600

    Initial commit, does not yet build/copy writer utility and scripts

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -35,6 +35,17 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     git checkout ${KERNEL_VERSION}
 
     # TODO: Add your kernel build steps here
+    # Lecture "Building the Linux kernel", slides 12-15
+    echo make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
+    echo make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
+    echo make -j8 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
+    make -j8 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
+    echo make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} modules
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} modules
+    echo make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
 fi
 
 echo "Adding the Image in outdir"
@@ -48,27 +59,57 @@ then
 fi
 
 # TODO: Create necessary base directories
+# Lecture "Linux root filesystems", slide 11
+mkdir -p ${OUTDIR}/rootfs
+cd ${OUTDIR}/rootfs
+mkdir -p bin dev etc home lib lib64 proc sbin sys tmp us var
+mkdir -p usr/bin usr/lib usr/sbin
+mkdir -p var/log
 
 cd "$OUTDIR"
+
 if [ ! -d "${OUTDIR}/busybox" ]
 then
 git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
     # TODO:  Configure busybox
+    # Lecture "Linux root filesystems", slide 14
+    echo make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} distclean
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} distclean
+    echo make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
 else
     cd busybox
 fi
 
 # TODO: Make and install busybox
+# Lecture "Linux root filesystems", slide 14
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
+
+cd ${OUTDIR}/rootfs
 
 echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
+# This directory is the last line of output from:
+# aarch64-none-linux-gnu-gcc -print-sysroot -v
+# Lecture "Linux root filesystems", slide 15
+LIBC_BASE_DIR=`which ${CROSS_COMPILE}gcc`
+LIBC_BASE_DIR=`dirname ${LIBC_BASE_DIR}`/../aarch64-none-linux-gnu/libc
+cp ${LIBC_BASE_DIR}/lib/ld-linux-aarch64.so.1 ${OUTDIR}/rootfs/lib
+cp ${LIBC_BASE_DIR}/lib64/libm.so.6 ${OUTDIR}/rootfs/lib64
+cp ${LIBC_BASE_DIR}/lib64/libresolv.so.2 ${OUTDIR}/rootfs/lib64
+cp ${LIBC_BASE_DIR}/lib64/libc.so.6 ${OUTDIR}/rootfs/lib64
 
 # TODO: Make device nodes
+# Lecture "Linux root filesystems", slide 16
+cd ${OUTDIR}/rootfs
+sudo mknod -m 666 dev/null c 1 3
+sudo mknod -m 600 dev/null c 5 1
 
 # TODO: Clean and build the writer utility
 
@@ -76,5 +117,14 @@ ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 # on the target rootfs
 
 # TODO: Chown the root directory
+# Lecture "Linux root filesystems", slide 17
+cd ${OUTDIR}/rootfs
+# May need to chown rootfs dir as well?
+sudo chown -r root:root *
 
 # TODO: Create initramfs.cpio.gz
+# Lecture "Linux root filesystems", slide 19
+cd ${OUTDIR}/rootfs
+find . | cpio -H newc -ov --owner root:root > cd ${OUTDIR}/initramfs.cpio
+cd ${OUTDIR}
+gzip -f initramfs.cpio
