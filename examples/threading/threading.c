94c896749fd250e31640597831344e63d7536413
Thomas Ames <tham7107@colorado.edu>
2023-07-10 23:27:43 -0600

    Implemented threading demo, passes unit tests

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -14,6 +14,30 @@ void* threadfunc(void* thread_param)
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    struct thread_data *pThreadData = (struct thread_data *) thread_param;
+
+    /* Non-zero return from usleep or pthread_mutex_lock/unlock indicates failure*/
+    if (usleep(pThreadData->wait_to_obtain_ms * 1000)) {
+	pThreadData->thread_complete_success = false;
+	return thread_param;
+    }
+
+    if (pthread_mutex_lock(pThreadData->mutex)) {
+	pThreadData->thread_complete_success = false;
+	return thread_param;
+    }
+
+    if (usleep(pThreadData->wait_to_release_ms * 1000)) {
+	pThreadData->thread_complete_success = false;
+	return thread_param;
+    }
+
+    if (pthread_mutex_unlock(pThreadData->mutex)) {
+	pThreadData->thread_complete_success = false;
+	return thread_param;
+    }
+
+    pThreadData->thread_complete_success = true;
     return thread_param;
 }
 
@@ -28,6 +52,23 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+    struct thread_data *pThreadData;
+
+    if (!(pThreadData = malloc(sizeof(struct thread_data)))) {
+	return false;
+    }
+
+    /* mutex is always initialized by test code before callign this func */
+    pThreadData->mutex                   = mutex;
+    pThreadData->wait_to_obtain_ms       = wait_to_obtain_ms;
+    pThreadData->wait_to_release_ms      = wait_to_release_ms;
+    pThreadData->thread_complete_success = false;
+
+    /* Returns 0 on success, non-zero on failure. attr==NULL for defaults */
+    if (pthread_create(thread, NULL, threadfunc, (void *) pThreadData)) {
+	return false;
+    } else {
+	return true;
+    }
 }
 
