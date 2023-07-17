07d52d3c06ba6c0043669fe28ad7ec079f979f3a
Thomas Ames <tham7107@colorado.edu>
2023-07-16 20:03:19 -0600

    Changes for Assignment 4 part 2

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -8,7 +8,9 @@ set -u
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
-username=$(cat conf/username.txt)
+#username=$(cat conf/username.txt)
+username=$(cat /etc/finder-app/conf/username.txt)
+FINDERRESULTFILE=/tmp/assignment4-result.txt
 
 if [ $# -lt 3 ]
 then
@@ -58,16 +60,17 @@ fi
 
 for i in $( seq 1 $NUMFILES)
 do
-	./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+	writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
-OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
+OUTPUTSTRING=$(finder.sh "$WRITEDIR" "$WRITESTR")
 
 # remove temporary directories
 rm -rf /tmp/aeld-data
 
 set +e
 echo ${OUTPUTSTRING} | grep "${MATCHSTR}"
+echo ${OUTPUTSTRING} > $FINDERRESULTFILE
 if [ $? -eq 0 ]; then
 	echo "success"
 	exit 0
